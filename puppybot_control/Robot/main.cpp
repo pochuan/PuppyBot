53084445718b78c5b943ce83929dc78b21bbe9c6
Jason Jong <jjong@stanford.edu>
2013-06-11 00:00:43 -0700

    Final version.
    Deleted a bunch of printf's and unused functions

--- puppybot_control/Robot/main.cpp
+++ puppybot_control/Robot/main.cpp
@@ -17,48 +17,15 @@ using namespace cv;
 int Hue = 40;
 int Sat = 150;
 int Val = 150;
-float RobotPos[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-int YCritThresh = 50;
-int counter = 0;
 RobotCom* robot;
 CvCapture* capture;
 
-
-void RotateRobot(double DegCCRot);
-IplImage* GetThresholdedImage(IplImage* img, CvScalar minHSV, CvScalar maxHSV);
-bool findBallPos(int* lastX, int* lastY );
-void CenterRobotOnBall(bool prec);
 bool ballInWorkspace();
-
-void RobotPositionReached() {
-  float pos[8];
-  while(true) {
-    bool reached = true;
-    robot->getStatus(GET_JPOS, pos);
-    for (int i=0; i <2; i++){
-      float diff = pos[i] - RobotPos[i];
-      if (fabs(diff) >= 0.05) {
-      //  printf("fail: %i %f\n", i, diff);
-        reached = false;
-      }
-    }
-    float diff = (pos[2]) - RobotPos[2];
-    if (fabs(diff) >= 0.003) {
-      reached = false;
-    }
-    for (int i=3; i <8; i++){
-      float diff = pos[i] - RobotPos[i];
-      if (i!=5  && fabs(diff) >= 0.05) {
-      //  printf("fail: %i %f\n", i, diff);
-        reached = false;
-      }
-    }
-
-    if (reached) break;
-    //printf("Position not reached %i\n", count);
-    sleep(0.25);
-  }
-}
+void MoveForwardToBall();
+void CenterRobotOnBall(bool prec);
+void RotateRobot(double DegCCRot);
+bool findBallPos(int* lastX, int* lastY);
+IplImage* GetThresholdedImage(IplImage* img, CvScalar minHSV, CvScalar maxHSV);
 
 bool findBallPos(int& posX, int& posY) {
   IplImage* frame = cvQueryFrame( capture );
@@ -68,8 +35,6 @@ bool findBallPos(int& posX, int& posY) {
     getchar();
     return false;
   }
-
-  // printf("Hue: %d Saturation: %d Value: %d \n", Hue, Sat, Val);
 
   CvScalar minHSV = cvScalar(Hue, Sat, Val);
   CvScalar maxHSV = cvScalar(Hue+20, 255, 255);
@@ -88,9 +53,7 @@ bool findBallPos(int& posX, int& posY) {
   posY = moment01/area;
 
   cvShowImage("thresh", imgThresh);
-  //cvShowImage( "video", frame );
   cvReleaseImage(&imgThresh);
-  //cvReleaseImage(&frame);
   delete moments;
 
   // Draw a line only if positions are valid
@@ -117,12 +80,7 @@ void RotateRobot(double degCCRot) {
   pos[1] = 0.0;
   pos[2] = radians;
 
-  for(int i=0; i<8; i++) {
-    RobotPos[i] = pos[i];
-  }
-
   robot->control(JTRACK, pos, 8);
-//  RobotPositionReached();
 }
 
 void CenterRobotOnBall(bool prec) {
@@ -147,7 +105,6 @@ void CenterRobotOnBall(bool prec) {
         RotateRobot(1.0);
       } else {
         // ball in the center!
-        printf ("Ball is center\n");
         break;
       }
     } else {
@@ -157,48 +114,24 @@ void CenterRobotOnBall(bool prec) {
   }
 }
 
-void RobotArmDown() {
-  printf("Rotate robot arm\n");
-  RobotPos[4] = 1.63;
-  RobotPos[6] = 1.63;
-  robot->control(JTRACK, RobotPos, 8);
-  RobotPositionReached();
-}
-
 void MoveForward(double dist) {
   if (dist == 0) return;
 
-  printf("Moving robot forward %f\n", dist);
-  // RobotPos[0] = RobotPos[0] +(float) dist;//*cos(RobotPos[2]);
   float pos[8];
   robot->getStatus(GET_JPOS, pos);
-  // pos[0] += ((float)dist)*cosf(pos[2]);
-  // pos[1] += ((float)dist)*sinf(pos[2]);
 
   pos[0] = (float) dist;
   pos[1] = 0.0;
   pos[2] = 0.0;
-  printf(" %f %f\n", pos[1], pos[2]);
 
-  for(int i=0; i<1; i++) {
-    RobotPos[i] = pos[i];
-  }
-  //pos[2] = RobotPos[2]; 
-
-  //RobotPos[2] = 0.0;
-  //RobotPos[1] = RobotPos[1] + dist*sin(RobotPos[2]);
   robot->control(JTRACK,pos, 8);
-  // robot->control(JTRACK,RobotPos, 8);
-//  RobotPositionReached();
 }
 
 void MoveForwardToBall() {
   int posX, posY;
-  printf("move Forward To Ball\n");
   while (!ballInWorkspace()) {
     findBallPos(posX, posY);
     float dist = 30*0.0254;
-    printf("Current posY: %i\n", posY);
     if (posY > 650) {
       float cube = posY*posY*posY;
       float sqr = posY*posY;
@@ -206,7 +139,6 @@ void MoveForwardToBall() {
       dist = 0.0254*dist;
     } 
     dist = dist / 10.0;
-    printf("Move Forward %f\n", dist);
     MoveForward(dist);
     CenterRobotOnBall(false);
   }
@@ -222,27 +154,9 @@ bool ballInWorkspace() {
   return false;
 }
 
-void FloatAndReportJointAngles() {
-  robot->control(FLOAT,RobotPos, 8);
-  printf("Float\n");
-  while(true) {
-    float data_in[8];
-    robot->getStatus(GET_JPOS, data_in);
-    std::cout<<"J:"<<data_in[0]<<" "<<data_in[1]<<std::endl;
-    std::cout<<data_in[2]<<" "<<data_in[3]<<std::endl;
-    std::cout<<data_in[4]<<" "<<data_in[5]<<std::endl;
-    std::cout<<data_in[6]<<" "<<data_in[7]<<std::endl;
-    sleep(2);
-  }
-}
-
 void PickUpBall() {
-  printf("Pick up ball\n");
-
-  printf("open gripper\n");
   robot->controlGripper( (float)10 );
   
-//  float RobotPos[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   float pos[8];
   robot->getStatus(GET_JPOS, pos);
   pos[0] = 0.0;
@@ -250,7 +164,6 @@ void PickUpBall() {
   pos[2] = 0.0;
 
   pos[4] = 0.5;
-  //RobotPos[4] = 0.5;
   robot->control(JTRACK, pos, 8);
   sleep(1.5);
   pos[3] = 6*3.14159/180.0;
@@ -259,33 +172,22 @@ void PickUpBall() {
   robot->control(JTRACK, pos, 8);
   sleep(3);
   
-  printf("Close Gripper\n");
   robot->controlGripper( (float)-5 );
   sleep(5);
 
-  printf("raise arm\n");
   pos[7] = 1.57;
   pos[6] = 0.0;
   robot->control(JTRACK, pos, 8);
   sleep(3);
-
-  for(int i=0; i<8; i++) {
-//    RobotPos[i] = pos[i];
-  }
 }
 
 void trackBall() {
     int frameWidth = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-    int frameHeight = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
     int frameCenterX = frameWidth/2;
-    int frameCenterY = frameHeight/2;
 
     int posX, posY;
     while(true) {
-        printf("++++++++++++");
-//        printf("findBallPos\n");
         bool onScreen = findBallPos(posX, posY);
-        printf(" X: %i Y:%i\n", posX, posY);
         if (posX < 0 || posY < 0) {
             continue;
         }
@@ -293,47 +195,35 @@ void trackBall() {
         bool d1 = false;
         bool d2 = false;
         if (!onScreen) {
-            printf("Ball not on screen, Rotate and search\n");
             MoveForward(0.0);
             RotateRobot(5.0);
         } else {
             if (posY > 1000) {
-                printf("Ball far away\n");
                 MoveForward(0.6);
             } else if (posY > 700) {
-                printf("Ball close\n");
                 MoveForward(0.05);
             } else if (posY < 680) {
-                printf("Ball too close\n");
                 MoveForward(-0.05);
             } else {
-                printf("Ball right disatance\n");
                 MoveForward(0.0);
                 d1 = true;
             }
             
-            printf("  %i > %i\n", posX, frameWidth);
-            printf("  %i < %i\n", posX, frameCenterX);
             if (posX > (frameCenterX + 100)) {
-                printf("Ball on left\n");
                 RotateRobot(5.0);
             } else if (posX < (frameCenterX - 100)) {
-                printf("Ball on right\n");
                 RotateRobot(-5.0);
             } else {
-                printf("Ball centered\n");
                 RotateRobot(0.0);
                 d2 = true;
             }
         }
         
         if (d1 && d2) {
-            printf("Ball in workspace\n");
             break;
         }
         
     }
-    printf("Pick up ball\n");
     CenterRobotOnBall(true);
     PickUpBall();
     RotateRobot(20);
@@ -364,63 +254,22 @@ int main(int argc, char** argv)
     exit(0);
   }
 
-/*  cvNamedWindow("video", CV_WINDOW_AUTOSIZE);
-  cvNamedWindow("thresh", CV_WINDOW_AUTOSIZE);
-
-  cvCreateTrackbar("Hue","thresh", &Hue, 245, NULL);
-  cvCreateTrackbar("Saturation","thresh", &Sat, 245, NULL);
-  cvCreateTrackbar("Value","thresh", &Val, 245, NULL);
-*/
   std::cout<<"Sucessfully connected!"<<std::endl;
 
   /****************************************/
   //TEST 2: SEND MESSAGE TO THE SERVO SERVER
   /***************************************
   */
-//  RotateRobot(5);
-//  sleep(2);
-//  RotateRobot(-5);
-//  sleep(1);
-//  MoveForward(0.01);
-//  sleep(1);
-//  MoveForward(0.0);
-//  exit(0);
-//   CenterRobotOnBall(false);
-//   MoveForwardToBall();
-    float pos[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    robot->control(JTRACK,pos, 8);
-    while (true) {
-      trackBall();
-      getchar();
-    }
-//  int posX, posY;
-//  while(true) {
-//    printf("findBallPos\n");
-//    bool onScreen = findBallPos(posX, posY);
-//    printf(" X: %i Y:%i\n", posX, posY);
-//    if (posY > 500 && posY < 680) {
-//      CenterRobotOnBall(true);
-//      PickUpBall();
-//      break;
-//    }
-//  }
 
-  printf("DONE ARM MOVEMENT\n");
-  while(true){}
-
-//  cvReleaseCapture( &capture );
-//  cvDestroyWindow("video"); // destroys all windows
-//  cvDestroyWindow("thresh"); // destroys all windows
+  float pos[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  robot->control(JTRACK,pos, 8);
+  while (true) {
+    trackBall();
+    getchar();
+  }
 
   /****************************************/
-  //TEST 3: RECEIVE REPLIES FROM THE SERVO SERVER
-  /****************************************/
-  float data_in[8];
-  robot->getStatus(GET_JPOS, data_in);
-  std::cout<<"Joint angles are "<<data_in[0]<<" "<<data_in[1]<<" "<<data_in[2]<<" "<<data_in[3]<<" "<<data_in[4]<<" "<<data_in[5]<<" "<<data_in[6]<<" "<<data_in[7]<<std::endl;
-
-  /****************************************/
-  //TEST 4: TEAR DOWN
+  //TEST 3: TEAR DOWN
   /****************************************/
   delete robot;
   std::cout<<"Successfully disconnected!"<<std::endl;
